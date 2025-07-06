4bb7d0f3cc5ad49ce8f0733f2e041b2a81192d7c
hashirscam <shkazmi.bscs21seecs@student.nust.edu.pk>
2025-07-06 17:32:35 +0500

    update: made updates to the code

--- src/personify/crew.py
+++ src/personify/crew.py
@@ -1,23 +1,23 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import SerperDevTool
+from crewai.agents.agent_builder.base_agent import BaseAgent
 from crewai.memory import LongTermMemory, ShortTermMemory, EntityMemory
 from crewai.memory.storage.rag_storage import RAGStorage
 from crewai.memory.storage.ltm_sqlite_storage import LTMSQLiteStorage
-
+from typing import List
 
 @CrewBase
 class Personify():
     """Personal Statement Writer Crew"""
 
-    agents_config = 'config/agents.yaml'
-    tasks_config = 'config/tasks.yaml'
+    agents: List[BaseAgent]
+    tasks: List[Task]
 
     @agent
     def personality_interviewer(self) -> Agent:
         return Agent(
             config=self.agents_config['personality_interviewer'],
-            tools=[SerperDevTool()],
             verbose=True,
             memory=True
         )
@@ -26,7 +26,6 @@ class Personify():
     def technical_interviewer(self) -> Agent:
         return Agent(
             config=self.agents_config['technical_interviewer'],
-            tools=[SerperDevTool()],
             verbose=True,
             memory=True
         )
@@ -116,37 +115,25 @@ class Personify():
             tasks=self.tasks, 
             process=Process.sequential,
             verbose=True,
-            memory=True,
-            # Long-term memory for persistent storage across sessions
-            long_term_memory = LongTermMemory(
-                storage=LTMSQLiteStorage(
-                    db_path="./memory/long_term_memory_storage.db"
-                )
-            ),
-            # Short-term memory for current context using RAG
-            short_term_memory = ShortTermMemory(
-                storage = RAGStorage(
-                        embedder_config={
-                            "provider": "openai",
-                            "config": {
-                                "model": 'text-embedding-3-small'
-                            }
-                        },
-                        type="short_term",
-                        path="./memory/"
-                    )
-                ),            # Entity memory for tracking key information about entities
-            entity_memory = EntityMemory(
-                storage=RAGStorage(
-                    embedder_config={
-                        "provider": "openai",
-                        "config": {
-                            "model": 'text-embedding-3-small'
-                        }
-                    },
-                    type="short_term",
-                    path="./memory/"
-                )
-            ),
+            # memory=True,
+            # # Long-term memory for persistent storage across sessions
+            # long_term_memory = LongTermMemory(
+            #     storage=LTMSQLiteStorage(
+            #         db_path="./memory/long_term_memory_storage.db"
+            #     )
+            # ),
+            # # Entity memory for tracking key information about entities
+            # entity_memory = EntityMemory(
+            #     storage=RAGStorage(
+            #         embedder_config={
+            #             "provider": "openai",
+            #             "config": {
+            #                 "model": 'text-embedding-3-small'
+            #             }
+            #         },
+            #         type="short_term",
+            #         path="./memory/"
+            #     )
+            # ),
         )
 
